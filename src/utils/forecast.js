89e7353a7038ec6229357e4deb7a2fb74ab89f8e
AMMAR AQLEH <ammar.n.aqleh@gmail.com>
2021-02-18 13:50:42 +0400

    Init Commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,17 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=21fa245d7d1a8c98237cfc44ba73858b&query=' + latitude + ',' + longitude
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (body.error) {
+            callback('Unable to find location', undefined)
+        } else {
+            callback(undefined, body.current.weather_descriptions[0] + ' It is currently ' + body.current.temperature + ' degress out. There is a ' + body.current.humidity + '% humidity ')
+        }
+    })
+}
+
+module.exports = forecast
