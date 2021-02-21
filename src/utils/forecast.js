3d09f53445774b043480836ccad454eabaeefad9
AMMAR AQLEH <ammar.n.aqleh@gmail.com>
2021-02-21 09:09:23 +0400

    Forcast updated with feelslikes and visiability

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,13 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + ' It is currently ' + body.current.temperature + ' degress out. There is a ' + body.current.humidity + '% humidity and visibility is ' + body.current.visibility + ' feelslike ' + body.current.feelslike)
+            callback(
+                undefined,
+                body.current.weather_descriptions[0] + ' It is currently ' +
+                body.current.temperature + ' degress out. There is a ' +
+                body.current.humidity + '% humidity and visibility is ' +
+                body.current.visibility + ' feelslike ' +
+                body.current.feelslike)
         }
     })
 }
